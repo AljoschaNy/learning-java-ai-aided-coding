075ea854bd8b679980d870e1a28b8f8e481fbeda
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-05 11:05:41 +0200

    Add reverseString method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String input = "Hallo";
+        String reversed = reverseString(input);
+        System.out.println(reversed);
+    }
+
+    public static String reverseString(String input) {
+        if (input == null) {
+            return null;
+        }
+        return new StringBuilder(input).reverse().toString();
     }
 }
