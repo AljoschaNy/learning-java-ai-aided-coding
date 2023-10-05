d2bc15f8b2047e032c14056fa8d06872b53e06b0
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-05 11:28:31 +0200

    Add generateFirstNPrimes method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         String input = "Hallo";
@@ -9,6 +12,10 @@ public class Main {
         String input2 = "Anna";
         boolean isPalindrome = isPalindrome(input2);
         System.out.println(isPalindrome ? "It's a palindrome." : "It's not a palindrome.");
+
+        int n = 10;
+        List<Integer> primes = generateFirstNPrimes(n);
+        System.out.println(primes);
     }
 
     public static String reverseString(String input) {
@@ -35,5 +42,29 @@ public class Main {
         String cleanedInput = input.toLowerCase();
         String reversed = new StringBuilder(cleanedInput).reverse().toString();
         return cleanedInput.equals(reversed);
+    }
+
+    public static List<Integer> generateFirstNPrimes(int n) {
+        List<Integer> primes = new ArrayList<>();
+        int num = 2;  // Start with the first prime number
+        while (primes.size() < n) {
+            if (isPrime(num)) {
+                primes.add(num);
+            }
+            num++;
+        }
+        return primes;
+    }
+
+    private static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
