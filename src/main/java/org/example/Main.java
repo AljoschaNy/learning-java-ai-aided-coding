d3b22a3dc6c4c701fbcd4988d2bb69669ad2c344
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-05 11:11:35 +0200

    Add isPalindrome method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,10 @@ public class Main {
         String input = "Hallo";
         String reversed = reverseString(input);
         System.out.println(reversed);
+
+        String input2 = "Anna";
+        boolean isPalindrome = isPalindrome(input2);
+        System.out.println(isPalindrome ? "It's a palindrome." : "It's not a palindrome.");
     }
 
     public static String reverseString(String input) {
@@ -12,5 +16,14 @@ public class Main {
             return null;
         }
         return new StringBuilder(input).reverse().toString();
+    }
+
+    public static boolean isPalindrome(String input) {
+        if (input == null) {
+            return false;
+        }
+        String cleanedInput = input.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        String reversed = new StringBuilder(cleanedInput).reverse().toString();
+        return cleanedInput.equals(reversed);
     }
 }
